90b7142c2ab35de73b0220293b7b0a1592f77b12
Mehul Raheja <81683759+MehulRaheja@users.noreply.github.com>
2022-02-27 15:07:38 +0530

    clutter removed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,7 @@
 import Axios from 'axios';
 
 export const signinRequest = async (email, password) => {
-    // const body = {
-    //     email : email,
-    //     password: password
-    // };
+   
     try {
         const graphqlQuery = {
             query: `
@@ -22,8 +19,6 @@ export const signinRequest = async (email, password) => {
         const result = await Axios.post(
             `${process.env.REACT_APP_BASE_URL}/graphql/`, graphqlQuery
         );
-        // console.log(result.data.data.login.token);
-        // const data =  await Axios.post(`${process.env.REACT_APP_BASE_URL}/user/login/`, body);
         return result.data.data.login;
     } catch (error) {
         console.log("API ERROR: "+error);
@@ -34,15 +29,7 @@ export const signinRequest = async (email, password) => {
 export const signupRequest = async (data) => {
     const formData = new FormData();
     formData.append('image', data[6]);
-    // const body = {
-    //     email : data[0],
-    //     password: data[1],
-    //     type: data[2],
-    //     name: data[3],
-    //     school: data[4],
-    //     std: data[5]
-    // };
-    // console.log(data);
+    
     
     
     try {
@@ -85,13 +72,7 @@ export const signupRequest = async (data) => {
         console.log("API ERROR: "+error);
         return error;
     }
-    // try {
-    //     const data =  await Axios.post(`${process.env.REACT_APP_BASE_URL}/user/signup/`, body);
-    //     return data;
-    // } catch (error) {
-    //     console.log("API ERROR: "+error);
-    //     return error;
-    // }
+    
 }
 
 export const fetchProfile = async (token) => {
@@ -119,18 +100,7 @@ export const fetchProfile = async (token) => {
         console.log("API ERROR: "+error);
         return error;
     }
-    // try {
-        
-    //     const data = await Axios.get(
-    //         `${process.env.REACT_APP_BASE_URL}/profile`, 
-    //         { headers: {"Authorization" : `Bearer ${token}`}}
-    //     );
-    //     console.log(data);
-    //     return data;
-    // } catch (error) {
-    //     console.log("API ERROR: "+error);
-    //     return error;
-    // }
+  
 }
 
 export const fetchUserdata = async (token) => {
@@ -175,16 +145,7 @@ export const fetchUserTestdata = async (token) => {
         console.log("API ERROR: "+error);
         return error;
     }
-    // try {
-    //     const data = await Axios.get(
-    //         `${process.env.REACT_APP_BASE_URL}/user/result`, 
-    //         { headers: {"Authorization" : `Bearer ${token}`}});
-    //         console.log(data);
-    //         return data;
-    // } catch (error) {
-    //     console.log("API ERROR: "+error);
-    //     return error;
-    // }
+    
 }
 
 export const fetchTestData = async () => {
@@ -286,23 +247,5 @@ export const submitUserTest = async (testId, subject, responseArray, token) => {
         console.log("API ERROR: "+error);
         return error;
     }
-    // try {
-    //     const data = await Axios.post(
-    //         `${process.env.REACT_APP_BASE_URL}/user/submit-test`, body,
-    //         { headers: {"Authorization" : `Bearer ${token}`}});
-    //     return data;
-    // } catch (error) {
-    //     console.log("API ERROR: "+error);
-    //     return error;
-    // }
+   
 }
-
-// 'login' is the name of the function defined in RootQuery and resolver in backend
-//  after login, entries inside {} are fields we want to get from backend
-//  For using Graphql: there is only one change in frontend i.e. body of the request and all the api methods are post request
-//  inside query: 2 types, query(read) or mutation(write, edit, delete ) can be done
-//  if we are not using variables we should not write keyword 'query' inside query object like we write 'mutation' because it will give an error
-//  to each query or mutation we give them a name e.g. in loginHandler we give query this name UserLogin. Now we can add this keyword 'query' and in the next parenthesis() we can specify our variables
-//  $ is important and graphql will know it is a dynamic value
-//  keywords inside graphqlQuery: 'query' and 'variables' should be named like this only because they are reserved for graphql
-//  variables should be named without $ sign
